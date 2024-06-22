e1a5c7113a162ddf40960f8ea8b76f3df446ea96
Garamon <ollietoh@gmail.com>
2024-06-22 02:06:27 -0400

    web files

---
+++ src/routes/ControlPanel.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+    import { transformations } from './stores';
+    import MatrixEditor from './MatrixEditor.svelte'
+</script>
+
+<div
+style:width="50%">
+    {#each $transformations as [id, matrix]}
+        <MatrixEditor bind:matrix={matrix}></MatrixEditor>
+        <div
+        style:color="black"
+        style:height="5px"
+        style:width="100%"
+        style:margin="20px"
+        style:border="5px"></div>
+    {/each}
+</div>
+
---
+++ src/routes/MatrixEditor.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    export let matrix: Float32Array;
+
+</script>
+
+<div 
+style:display="grid"
+style:grid-template-columns="repeat(4, 1fr)"
+style:grid-template-rows="repeat(3, 1fr)">
+    {#each matrix as value}
+        <div>
+            <input type="number" bind:value={value} min="-1" max="1" step=0.1/>
+            <input type="range" bind:value={value} min="-1" max="1" step=0.1/>
+        </div>
+    {/each}
+</div>
